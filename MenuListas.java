e84d187982b13aa63e3dd0d5d914ae6256b2d8e4
daniel21connor <jconnorr1@miumg.edu.gt>
2022-06-05 07:38:25 +0200

    Se agrego el metodo buscar de listas y su respectiva clase menu donde se mandara a llamar todos sus datos

---
+++ MenuListas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package menprincipal;
+
+/**
+ *
+ * @author PC GAMING
+ */
+public class MenuListas {
+    
+}
