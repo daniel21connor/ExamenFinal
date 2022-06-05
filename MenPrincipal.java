1cc4582979b427b7d11cca0c12518893750de462
daniel21connor <jconnorr1@miumg.edu.gt>
2022-06-05 07:59:28 +0200

    se agrego todo al menu principal, da por finalizadoe l programa

--- MenPrincipal.java
+++ MenPrincipal.java
@@ -1,11 +1,45 @@
 
 package menprincipal;
 
+import static menprincipal.MetodoListas.scanner;
+
 public class MenPrincipal {
 
  
     public static void main(String[] args) {
-      
+         MenuListas hola=new MenuListas();
+   MenuArrays hi=new MenuArrays();
+         int opcion  = 0;
+         
+  do{
+        System.out.println("\n BIENVENIDO A GUATEMALA:");
+          System.out.println("<<<<>>>>>>>>>>>>>>>>>>");
+           System.out.println(" 1"+" METODO LISTAS");
+            System.out.println(" 2"+" METODO ARRAYS");
+             System.out.println(" 3"+" SALIR");
+           System.out.println(" "+"Escoja el metodo que desea");
+           System.out.println("<<<<>>>>>>>>>><<<>>>>>>>>>>>>>>>>><<<<>>");
+          
+        opcion = scanner.nextInt();
+       switch(opcion){
+               
+         case 1 -> { 
+           hola.menuLista(opcion);
+            
+           
+               }
+          
+         case 2 -> {
+       hi.menuLista(opcion);
+         }  
+                   
     }
     
-}
+}while (opcion !=3);
+    }
+    
+    
+
+    }
+    
+
